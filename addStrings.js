5cca9e6565b08391fcc1dc5c7fed35dc42164c43
Skilldvirus <30620387+Skilldvirus@users.noreply.github.com>
2022-04-09 10:41:43 +0530

    Update addStrings.js

--- addStrings.js
+++ addStrings.js
@@ -1,32 +1,39 @@
-function add(stringNumbers){
+function add(stringNumbers)
+    {
+        const char = '//';
+        if(stringNumbers.startsWith(char))
+        {
 
-  if(stringNumbers.startWith("//")){
-       const char = '//';
-  if(stringNumbers.startsWith(char)){
-      let delimiter;
-    let numbersWithoutDelimiter = stringNumbers;
-      let delimiterIndex = stringNumbers.indexOf("//") + 2;
-      delimiter = stringNumbers.substring(delimiterIndex, delimiterIndex + 1);
-      numbersWithoutDelimiter = stringNumbers.substring(stringNumbers.indexOf("n") + 1);
+          let delimiter;
+          let numbersWithoutDelimiter = stringNumbers;
+          let delimiterIndex = stringNumbers.indexOf("//") + 2;
 
-let nuArray = numbersWithoutDelimiter.split(delimiter).map(n => parseInt(n, 10));
-if(nuArray.some(n => Number.isNaN(n))) console.log("0");
-  if(nuArray.some(n => n < 0)) console.log('Negatives not allowed');
-  return nuArray.reduce((s, n) => s + n)
-  }
-  else {
-  const numbers = stringNumbers
-    .replace(/\r?\n/g, ',') // Normalize delimiter
-    .split(',')                     // Split values
-    .map(n => parseInt(n, 10))      // Convert to integers
+          delimiter = stringNumbers.substring(delimiterIndex, delimiterIndex + 1);
+          numbersWithoutDelimiter = stringNumbers.substring(stringNumbers.indexOf("n") + 1);
 
-  // Check if numbers are ok.
+          let nuArray = numbersWithoutDelimiter.split(delimiter).map(n => parseInt(n, 10)) 
+          return checkNan_NonNegative(nuArray);
+
+       }
+        else 
+        {
+          const numbers = stringNumbers
+          .replace(/\r?\n/g, ',') 
+          .split(',')  
+          .map(n => parseInt(n, 10))  
+
+          return checkNan_NonNegative(numbers);
   
-  if(numbers.some(n => Number.isNaN(n))) console.log("0");
-  if(numbers.some(n => n < 0)) console.log('Negatives not allowed')
+         }
+   }
 
-  // Add them all up
-  return numbers.reduce((s, n) => s + n)
-  }
-}
-add("//;n3;6;15");
+    function checkNan_NonNegative(numArray)
+      {
+        if(numArray.some(n => Number.isNaN(n))) console.log("0");
+        if(numArray.some(n => n < 0)) console.log('Negatives not allowed')
+
+        // Add them all up
+        return numArray.reduce((s, n) => s + n) 
+      }
+
+add("//*n3*6*15*1");
